4afa88282a8061bac376631fe604587a3dc9a833
Jose Eduardo Monteiro <josee.edumont@gmail.com>
2023-06-01 00:31:21 -0300

    fix models

--- src/main/java/ufrn/imd/investmentservice/models/Poupanca.java
+++ src/main/java/ufrn/imd/investmentservice/models/Poupanca.java
@@ -2,12 +2,14 @@ package ufrn.imd.investmentservice.models;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedList;
 
-private class Historico {
+class Historico {
     private LocalDate data;
     private BigDecimal valorInserido;
 }
@@ -15,12 +17,20 @@ private class Historico {
 @Data
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "poupanca")
 public class Poupanca implements Investimento {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private int taxaDeJuros;
     private BigDecimal montante;
     private LinkedList<Historico> historico;
 
     public void retirarMontante() {}
+
+    public BigDecimal obterSaldo() {
+        return this.getMontante();
+    };
+    public void devolverInvestimento() {};
 }
--- src/main/java/ufrn/imd/investmentservice/models/TesouroDireto.java
+++ src/main/java/ufrn/imd/investmentservice/models/TesouroDireto.java
@@ -5,7 +5,8 @@ import java.math.BigDecimal;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedList;
@@ -13,8 +14,18 @@ import java.util.LinkedList;
 @Data
 @Builder
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tesouro_direto")
 public class TesouroDireto implements Investimento {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private BigDecimal saldo;
     private Integer taxaDeJuros;
+
+    public BigDecimal obterSaldo() {
+        return this.getSaldo();
+    };
+
+    public void devolverInvestimento() {};
 }
